51581cce14be03b9af81a183206a4b9e007f8dcf
Engramar Bollas <engramar.bollas@gmail.com>
2020-11-24 21:15:42 +1100

    Update js_2_6-10.js

--- js_2_6-10.js
+++ js_2_6-10.js
@@ -23,8 +23,20 @@ const magic = () => {
 };
 
 #Write Arrow Functions with Parameters
-
-
+const myConcat = (arr1, arr2) => {
+  "use strict";
+  return arr1.concat(arr2);
+};
+console.log(myConcat([1, 2], [3, 4, 5]));
 
 #Set Default Parameters for Your Functions
+// Only change code below this line
+const increment = (number, value=1) => number + value;
+// Only change code above this line
+
+
 #Use the Rest Parameter with Function Parameters
+const sum = (...args) => {
+  return args.reduce((a, b) => a + b, 0);
+}
+
